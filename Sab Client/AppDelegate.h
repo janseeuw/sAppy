aa0a1fcf9cc55443e47deb98489c35be851154eb
wixon <jonas.anseeuw@gmail.com>
2013-09-29 17:25:36 +0200

    Initial Commit

---
+++ Sab Client/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sab Client
+//
+//  Created by Jonas Anseeuw on 29/09/13.
+//  Copyright (c) 2013 Jonas Anseeuw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sab ClientTests/Sab_ClientTests.m
@@ -0,0 +1,34 @@
+//
+//  Sab_ClientTests.m
+//  Sab ClientTests
+//
+//  Created by Jonas Anseeuw on 29/09/13.
+//  Copyright (c) 2013 Jonas Anseeuw. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sab_ClientTests : XCTestCase
+
+@end
+
+@implementation Sab_ClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
